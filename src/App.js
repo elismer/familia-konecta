f2180edb161e8bf5465a2fefe8cbbf0438a162d3
Santiago Cuello <sjcuello@gmail.com>
2021-10-22 02:57:11 -0300

    Develop (#11)
    * Changes in Layout and List (#3)
    
    * Se crea el maquetado del listado de los mas votados (#4)
    
    * Update package-lock.json
    
    * add gitignore
    
    * Login page
    
    * Fix Graphql
    
    * Adding Approval page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { NotRegistered } from './pages/NotRegistered'
 import Context from './Context'
 import { Banner } from './components/Banner'
 import TermConditions from './pages/TermsConditions'
+import { ApprovePhotos } from './containers/ApprovePhotos'
 
 export default function () {
   return (
@@ -25,6 +26,7 @@ export default function () {
                   <Home path='/pet/:id' />
                   <Detail path='/detail/:id' />
                   <TermConditions path='/ttcc' />
+                  <ApprovePhotos path='/approve'/>
                 </Router>
               </>
               : <Router>
